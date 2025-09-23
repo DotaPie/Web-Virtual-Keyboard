4c0e2fb6644b199cd5d8374fa2c086641434cccb
Peter Čižmár (DotaPie) <pcizmar95@gmail.com>
2025-09-23 19:14:04 +0200

    Some cleanup

---
+++ platformio/include/config.h
@@ -0,0 +1,14 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define MASTER_USER      "admin"           	// <-- CHANGE THIS      
+#define MASTER_PASS      "password"    		// <-- CHANGE THIS
+#define WIFI_SSID        "some-wifi"		// <-- CHANGE THIS
+#define WIFI_PASS        "some-password"	// <-- CHANGE THIS
+#define TYPE_DELAY_MS    40
+#define UART_RX_PIN      44
+#define UART_TX_PIN      43
+#define UART_BAUD        115200
+#define HOSTNAME         "web-virtual-keyboard"
+
+#endif
